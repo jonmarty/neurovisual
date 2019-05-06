cc006891bf8f260161ba3bb502d8f7d8d4dfc9d9
jonmarty <jonathan.n.marty@gmail.com>
2019-05-05 23:41:49 -0400

    Got interactive expression box for stimulus and set of points showing how the stimulus varies with time

--- demos/neuro.js
+++ demos/neuro.js
@@ -2,9 +2,9 @@
 
 //Functions
 //TODO: Find TANH and COSH functions in JS
-let M_ss = function(data){return (1/2) * (1 + np.tanh((data.V - data.V_1) / data.V_2))}
-let N_ss = function(data){return (1/2) * (1 + np.tanh((data.V - data.V_3) / data.V_4))}
-let T_N  = function(data){return 1 / (data.phi * np.cosh((data.V - data.V_3) / (2 * data.V_4)))}
+let M_ss = function(data){return (1/2) * (1 + Math.tanh((data.V - data.V_1) / data.V_2))}
+let N_ss = function(data){return (1/2) * (1 + Math.tanh((data.V - data.V_3) / data.V_4))}
+let T_N  = function(data){return 1 / (data.phi * Math.cosh((data.V - data.V_3) / (2 * data.V_4)))}
 
 //Define differential equations
 let dV = function(data, I){return (I - data.g_L * (data.V - data.V_L) - data.g_Ca * M_ss(data) * (data.V - data.V_Ca) - data.g_K * data.N * (data.V - data.V_K)) / data.C}
@@ -12,10 +12,9 @@ let dN = function(data){return (N_ss(data) - data.N) / T_N(data)}
 
 //Update function
 let update = function(data,I){
-	data.x++
-	data.V += dV(data,I) * dt
-	data.N += dN(data) * dt
-	data.t += t
+	data.V += dV(data,I) * data.dt
+	data.N += dN(data) * data.dt
+	data.t += data.dt
 	data.x++
 }
 
@@ -27,27 +26,44 @@ let square = function(t, start, stop, intensity){
 	return 0
 }
 
+let on_off = function(t, on, off, intensity){
+	tn = t % (on+off)
+	if(tn <= on){
+		return intensity
+	}
+	return 0
+}
+
 //Graphics
 const x_i = -400;
-const y_i = -400;
+const y_i = -200;
+const M = 200; // Amount to multiply the voltage and recovery variable by for graphics
+const N = 1000; // Number of data points to store
+const n = 10; // Number of data points to display
+const plot_x = 400 // X value of bottom left of Current Data Plot
+const plot_y = -200 // Y value of bottom left of Current Data Plot
+const plot_width = 400 // Width of Current Data Plot
+const plot_height = 100 // Height of Current Data Plot
+var I_hist = new Array(N).fill(0)
 
 var data = {
 	x:x_i,
 	y:y_i,
 	t:0,
-	data:0,
-	C:0,
-	g_L:0,
-	g_Ca:0,
-	g_K:0,
-	V_L:0,
-	V_Ca:0,
-	V_K:0,
-	V_1:0,
-	V_2:0,
-	V_3:0,
-	V_4:0,
-	phi:0,
+	dt:0.01,
+	//I:Array(N).fill(0),
+	C:1,
+	g_L:1,
+	g_Ca:1,
+	g_K:1,
+	V_L:1,
+	V_Ca:1,
+	V_K:1,
+	V_1:1,
+	V_2:1,
+	V_3:1,
+	V_4:1,
+	phi:1,
 	V:0,
 	N:0
 }
@@ -67,19 +83,41 @@ var demo = g9(data, function(data,ctx) {
 	//Axon
 	ctx.line(-50,20,-400,20, lineconf)
 	ctx.line(-50,-20,-400,-20, lineconf)
-
-	fill = 'rgb('+Math.abs(data.x-x_i)+',0,'+Math.abs(data.y-y_i)+')';
+	
+	//Neuron
+	fill = 'rgb('+Math.abs(M*data.N)+',0,'+Math.abs(M*data.V)+')';
 	ctx.point(0, 0, {r:100, fill:fill});
-	ctx.point(data.x, data.y, {r:15, fill:fill});
+	ctx.point(x_i + M*data.N, y_i - M*data.V, {r:15, fill:fill});
+
+	//Graph of current points
+	/*var I_disp = new Array(n)
+	for (i=0;i<=n;i=i+1){
+		I_disp[i] = I_hist[n*i]
+	}
+	var I_max = Math.max(...I_disp)
+	var I_min = Math.min(...I_disp)
+	var I_range = I_max - I_min*/
+	for (i=0;i<=N;i=i+(N/n)){
+		ctx.point(plot_x + (i/N) * plot_width, plot_y - plot_height * I_hist[i], {r:plot_width/n/10})
+	}
 
 });
 
 setInterval(function(){
 	if(!demo.isManipulating){
 		var data = demo.getData()
-		I = square(data.t,0,200,1)
-		console.log(I)
+		var t = data.t
+		var sin = Math.sin
+		var pi = Math.PI
+		stim_expr = document.getElementById("stim_expr").value
+		try{
+			var I = eval(stim_expr)
+			if(I==undefined || typeof I != 'number'){I = 0}
+		}
+		catch(error){var I = 0}
 		update(data,I)
+		I_hist.pop()
+		I_hist.unshift(I)
 		demo.setData(data)
 	}
 }, 10)
